5d7852ca7e6db3cda56c2b6848745c92601422f0
kawakuboE <108171998+kawakuboE@users.noreply.github.com>
2022-12-02 11:25:23 +0900

    pdo.php修正

--- PHPjob/4-2/pdo.php
+++ PHPjob/4-2/pdo.php
@@ -4,16 +4,10 @@
     define('DB_PASSWORD', 'root');
     define('PDO_DSN', 'mysql:host=localhost;charset=utf8;dbname='.DB_DATABASE);
     
-    function db_connect() {
-        try {
-            // PDOインスタンスの作成
-            $pdo = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
-            // エラー処理方法の設定
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $pdo;
-        } catch(PDOException $e) {
-            echo 'Error: ' . $e->getMessage();
-            die();
-        }
+    try {
+        $dbh = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
+    } catch (PDOException $e) {
+        echo 'Error:' . $e->getMessage();
+        die();
     }
     ?>
